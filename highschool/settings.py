fd91c4bd7b411e24ab9bc1060e3c9b63756d6ab4
Ruby-Enthusiast <singed83@gmail.com>
2023-12-02 07:12:46 +0900

    Hackathon Frontend

--- highschool/settings.py
+++ highschool/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-08!2fheew-(w=$@jcunv4bn%q*qh1+3s@93p5=^kuaf2n$q3aa"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
